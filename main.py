6913cbac68b8a9fad68e768e432ade0402ac5830
evgenyseledkov <evgenyseledkov@gmail.com>
2024-02-12 20:48:04 +0800

    Add fine tuning fixes

--- main.py
+++ main.py
@@ -16,51 +16,62 @@ st.divider()
 
 col1_1, emp1_1, col2_1 = st.columns([3, 1, 3])
 with col1_1:
-    st.write("<h3><font color='#66cccc'><font size='5'><font face='Tahoma, Geneva, sans-serif'>Это Женя "
+    st.write("<h3 align='center'><font color='#66cccc'><font size='5'><font face='Tahoma, Geneva, sans-serif'>Это Женя "
              "&#128526;</font></font></font></h3>", unsafe_allow_html=True)
     st.image(f"images/Igen.jpg")
 
 with col2_1:
-    st.write("<h3><font color='#66cccc'><font size='5'><font face='Tahoma, Geneva, sans-serif'>А это Аня "
+    st.write("<h3 align='center'><font color='#66cccc'><font size='5'><font face='Tahoma, Geneva, sans-serif'>А это Аня "
                  "&#128131;</font></font></font></h3>", unsafe_allow_html=True)
     st.image(f"images/Park.jpg")
 
 st.divider()
 
-st.title("А дальше будут Мы")
-st.title("")
+st.title("А дальше будут :blue[фото]!")
+st.subheader("Просто напомню тебе, чем :rainbow[МЫ] "
+             "занимались последние 3 года :man_and_woman_holding_hands:", anchor=False)
+
+st.divider()
+
 
 col1, emp1, col2, emp2, col3 = st.columns([3, 1, 3, 1, 3])
 
 with col1:
+    st.header(":blue[Часть 1] :arrow_down:", divider=True)
     for index, row in df[:col1_pos].iterrows():
         date = str(row['date'])
         location = str(row['location'])
-        description = str(row['description']).capitalize()
+        description = str(row['description'])
         st.subheader(f"{date}: {location}")
         st.text(description)
         st.image(f"images/{row['file']}")
         st.divider()
 
 with col2:
+    st.header(":blue[Часть 2] :arrow_down:", divider=True)
     for index, row in df[col1_pos:col2_pos].iterrows():
         date = str(row['date'])
         location = str(row['location'])
-        description = str(row['description']).capitalize()
+        description = str(row['description'])
         st.subheader(f"{date}: {location}")
         st.text(description)
         st.image(f"images/{row['file']}")
         st.divider()
 
 with col3:
+    st.header(":blue[Часть 3] :arrow_down:", divider=True)
     for index, row in df[col2_pos:].iterrows():
         date = str(row['date'])
         location = str(row['location'])
-        description = str(row['description']).capitalize()
+        description = str(row['description'])
         st.subheader(f"{date}: {location}")
         st.text(description)
         st.image(f"images/{row['file']}")
         st.divider()
 
-st.write("А сколько всего я еще не показал! А сколько всего еще будет!")
-st.write("Я тебя люблю! :heart:")
+st.write("<address style='text-align: center;'><font color='#333399'><font size='4'><samp>"
+         "А сколько всего я еще не показал! А сколько всего еще будет!</samp></font></font></address>",
+         unsafe_allow_html=True)
+st.write("<address style='text-align: center;'><font size='6'><font color='#333399'><samp>"
+         "Я тебя &#129655;!</samp></font></font></address>",
+         unsafe_allow_html=True)
